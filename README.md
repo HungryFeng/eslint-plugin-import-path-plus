8209fb03fd72bdc5e77e7a1f29bf332bf843c203
jiang.feng <jiang.feng@huolala.cn>
2022-07-07 14:37:03 +0800

    feat: 🎸 init

---
+++ README.md
@@ -0,0 +1,46 @@
+# eslint-plugin-import-path-plus
+
+import path rules
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-import-path-plus`:
+
+```sh
+npm install eslint-plugin-import-path-plus --save-dev
+```
+
+## Usage
+
+Add `import-path-plus` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "import-path-plus"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "import-path-plus/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
---
+++ tests/lib/rules/no-internal-modules.js
@@ -0,0 +1,89 @@
+/**
+ * @fileoverview no internal modules
+ * @author Hungry
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-internal-modules'),
+  RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2020, sourceType: 'module' } });
+const options = [
+  {
+    target: '**/.umi/**',
+    replace: 'umi'
+  }
+];
+ruleTester.run('no-internal-modules', rule, {
+  valid: [
+    { code: "import { useRequest } from 'umi'", options },
+    { code: "import { useModel } from 'umi'", options },
+    { code: "import { useRequest, useModel } from 'umi'", options }
+  ],
+
+  invalid: [
+    {
+      code: "import { useRequest } from '@/.umi/request-plugin/request';",
+      errors: [{ messageId: 'noInternalModules' }],
+      options,
+      output: "import { useRequest } from 'umi';"
+    },
+    {
+      code: "import { useModel } from '@/.umi/model-plugin/model';",
+      errors: [{ messageId: 'noInternalModules' }],
+      options,
+      output: "import { useModel } from 'umi';"
+    },
+    {
+      code: "import { useRequest } from 'src/.umi/request-plugin/request';",
+      errors: [{ messageId: 'noInternalModules' }],
+      options,
+      output: "import { useRequest } from 'umi';"
+    },
+    {
+      code: "import { Link, useModel } from 'umi';",
+      errors: [{ messageId: 'noInternalModules' }],
+      options: [
+        {
+          target: 'umi',
+          replace: '@utils/router',
+          modules: ['Link', 'useHistory']
+        }
+      ],
+      output: "import {  useModel } from 'umi';\nimport { Link } from '@utils/router';"
+    },
+    // {
+    //   code: "import { Link, useModel, useHistory } from 'umi';",
+    //   errors: [{ messageId: 'noInternalModules' }, { messageId: 'noInternalModules' }],
+    //   options: [
+    //     {
+    //       target: 'umi',
+    //       replace: '@utils/router',
+    //       modules: ['Link', 'useHistory']
+    //     }
+    //   ],
+    //   output:
+    //     "import {  useModel,  } from 'umi';\nimport { Link } from '@utils/router';\nimport { useHistory } from '@utils/router';"
+    // }
+    // {
+    //   code: "import { useModel, Link, useHistory } from 'umi';",
+    //   errors: [{ messageId: 'noInternalModules' }, { messageId: 'noInternalModules' }],
+    //   options: [
+    //     {
+    //       target: 'umi',
+    //       replace: '@utils/router',
+    //       modules: ['Link', 'useHistory']
+    //     }
+    //   ],
+    //   output: "import { useModel } from 'umi';\nimport { Link, useHistory } from '@utils/router';"
+    // }
+  ]
+});
